df163dc3625814b0afba3c8f7e073b771cf25420
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2025-10-20 11:17:06 +0200

    if - else - plnolety

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// uživatel zadá věk
+// vypište zda je uživatel plnoletý nebo ne
+
+Console.WriteLine("zadej věk:");
+
+string input = Console.ReadLine();
+
+int age = int.Parse(input);
+
+if(age >= 18)
+    Console.WriteLine("Jste plnoletý/á.");
+else
+    Console.WriteLine("Nejste plnoletý/á.");
