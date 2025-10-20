810ed31fca083c27a3108d9a110cd2b051a5e8ec
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2025-10-20 14:06:34 +0200

    metody

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,11 +1,1 @@
 ﻿
-var result = FromCelsiusToFahrenheit(10.5);
-
-Console.WriteLine($"10.5 C = {result} F");
-
-double FromCelsiusToFahrenheit(double celsius)
-{
-    double fahrenheit = (celsius * 9 / 5) + 32;
-
-    return fahrenheit;
-}
