363704c25f7697ae1a477f39e77c47867e683397
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2025-10-20 13:12:50 +0200

    while cteni z konzole dokud + suma

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,61 +1,18 @@
-﻿// den v týdnu pomocí switch
-// 1-7 = pondělí - neděle
-// pokud je číslo mimo tento rozsah, vypiš "Neplatný den"
+﻿
 
-Console.WriteLine("Zadej číslo 1-7 a zmáčkni enter:");
+Console.WriteLine("Zadej číslo nebo 'Q' pro ukončení:");
 
-string? line = Console.ReadLine();
+int sum = 0;
+string line = Console.ReadLine();
 
-int dayNumber = int.Parse(line);
-
-//classic switch statement
-switch (dayNumber)
+while(line != "Q")
 {
-    case 1:
-        Console.WriteLine("Pondělí");
-        break;
-    case 2:
-        Console.WriteLine("Úterý");
-        break;
-    case 3:
-        Console.WriteLine("Středa");
-        break;
-    case 4:
-        Console.WriteLine("Čtvrtek");
-        break;
-    case 5:
-        Console.WriteLine("Pátek");
-        break;
-    case 6:
-        Console.WriteLine("Sobota");
-        break;
-    case 7:
-        Console.WriteLine("Neděle");
-        break;
-    default:
-        Console.WriteLine("číslo mimo rozsah!");
-        break;
+    int number = int.Parse(line);
+    sum += number;
+
+    Console.WriteLine($"Aktuální suma je: {sum}");
+    Console.WriteLine("Zadej číslo nebo 'Q' pro ukončení:");
+    line = Console.ReadLine();
 }
 
-//switch expression
-string denVTydnu = dayNumber switch
-{
-    1 => "Pondělí",
-    2 => "Úterý",
-    3 => "Středa",
-    4 => "Čtvrtek",
-    5 => "Pátek",
-    6 => "Sobota",
-    7 => "Neděle",
-    _ => "Neplatný den"
-};
-
-Console.WriteLine($"den v tydnu je: {denVTydnu}");
-
-//switch expression s podmínkami - pattern matching
-string jePracovniDen = dayNumber switch
-{
-    int i when i >= 1 && i <= 5 => "Pracovní den",
-    int i when i >= 6 && i <= 7 => "Víkend",
-    _ => "Neplatný den"
-};
+Console.WriteLine($"Konec programu, dosažená suma: {sum}");
