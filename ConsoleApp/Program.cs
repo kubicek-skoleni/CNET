e4027e470bd96133026f8c1a34f4fce9786a4b0c
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2025-10-20 11:27:47 +0200

    switch dny v tydnu

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,37 @@
 ﻿// den v týdnu pomocí switch
 // 1-7 = pondělí - neděle
 // pokud je číslo mimo tento rozsah, vypiš "Neplatný den"
+
+Console.WriteLine("Zadej číslo 1-7 a zmáčkni enter:");
+
+string? line = Console.ReadLine();
+
+int dayNumber = int.Parse(line);
+
+switch (dayNumber)
+{
+    case 1:
+        Console.WriteLine("Pondělí");
+        break;
+    case 2:
+        Console.WriteLine("Úterý");
+        break;
+    case 3:
+        Console.WriteLine("Středa");
+        break;
+    case 4:
+        Console.WriteLine("Čtvrtek");
+        break;
+    case 5:
+        Console.WriteLine("Pátek");
+        break;
+    case 6:
+        Console.WriteLine("Sobota");
+        break;
+    case 7:
+        Console.WriteLine("Neděle");
+        break;
+    default:
+        Console.WriteLine("číslo mimo rozsah!");
+        break;
+}
