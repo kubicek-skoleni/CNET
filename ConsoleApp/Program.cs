8733ca703a9f5157c0e83821f5da997e7ecdcbb8
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2025-10-21 09:18:37 +0200

    opakovani otazky den 1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int dayNumber = 3;
+
+//switch expression
+string denVTydnu = dayNumber switch
+{
+    1 => "Pondělí",
+    2 => "Úterý",
+    3 => "Středa",
+    4 => "Čtvrtek",
+    5 => "Pátek",
+    6 => "Sobota",
+    7 => "Neděle",
+    _ => "Neplatný den"
+};
