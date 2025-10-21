10010c64026c6c58d385cbca91f68c9bceaf69fc
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2025-10-21 15:00:37 +0200

    todo app - seznamUkolu trida, refaktoring, pozna kdy jsou splnene

---
+++ ConsoleApp/Model/SeznamUkolu.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace ConsoleApp.Model
+{
+    public class SeznamUkolu
+    {
+        // constructor
+        public SeznamUkolu(string soubor)
+        {
+            //načtení ze souboru
+            string[] radky = File.ReadAllLines("ukoly.txt");
+
+            foreach (string radek in radky)
+            {
+                Ukol novyUkol = new Ukol();
+                novyUkol.Popis = radek;
+                Ukoly.Add(novyUkol);
+            }
+        }
+        //seznam úkolů
+        public List<Ukol> Ukoly = new();
+
+
+        public void VypisUkoly()
+        {
+            foreach (var ukol in Ukoly)
+            {
+                int i = Ukoly.IndexOf(ukol);
+                Console.WriteLine($"{i + 1}: {ukol.Zobrazit()}");
+            }
+
+            Console.WriteLine();
+        }
+
+        public void OznamitSplneni(int cisloUkolu)
+        {
+            Ukoly[cisloUkolu - 1].Hotovo = true;
+        }
+
+        public bool JeHotovo()
+        {
+            foreach(var ukol in Ukoly)
+            {
+                if (ukol.Hotovo == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
