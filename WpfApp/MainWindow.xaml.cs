0a280afe86f7ce17ffb2c9ea49d9174f222f0f18
kubicek-skoleni <82667968+kubicek-skoleni@users.noreply.github.com>
2025-10-22 13:19:15 +0200

    tuple, todo try

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -21,11 +21,41 @@ namespace WpfApp
             InitializeComponent();
         }
 
+        byte counter = byte.MaxValue;
+
         private void btnOk_Click(object sender, RoutedEventArgs e)
         {
-            txtInfo.Text = "Hello World!";
+            //txtInfo.Text = int.MaxValue.ToString("N");
 
-            rctBox1.Fill = Brushes.Cornsilk;
+            counter++;
+
+            txtInfo.Text = counter.ToString();
+
+
+            //txtInfo.Text = "Hello World!";
+
+            //if(rctBox1.Fill == Brushes.Cornsilk)
+            //    rctBox1.Fill = Brushes.LightBlue;
+            //else
+            //    rctBox1.Fill = Brushes.Cornsilk;
+
+            int nahodny_cislo = Random.Shared.Next(0, 11);
+
+            rctBox1.Fill = nahodny_cislo switch
+            {
+                0 => Brushes.Cornsilk,
+                1 => Brushes.LightBlue,
+                2 => Brushes.LightGreen,
+                3 => Brushes.LightPink,
+                4 => Brushes.LightGray,
+                5 => Brushes.LightYellow,
+                6 => Brushes.Orange,
+                7 => Brushes.Violet,
+                8 => Brushes.LightCoral,
+                9 => Brushes.Aquamarine,
+                10 => Brushes.Gold,
+                _ => Brushes.White
+            };
         }
     }
 }
