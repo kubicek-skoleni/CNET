d56c9664156ff7077507ba7dad669b23458a518d
Kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-11-25 11:20:33 +0100

    nacteni datasetu

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -1,41 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ConsoleApp;
+﻿using System.Text.Json;
+using Model;
 
-Console.WriteLine("Hello, World!");
+string file = @"data2024.json";
 
+var file_content = File.ReadAllText(file);
 
-string[] words = { "apple", "banana", "cherry" };
+List<Person> persons = JsonSerializer
+                .Deserialize<List<Person>>(file_content)!;
 
-var stud = new Student()
-{
-    Name = "John Doe",
-    //Age = 20
-};
-
-stud.Name = "  ";
-
-List<int> numbers = [ 10, 50, -22 ];
-
-
-
-int tempInFahrenheit = 45;
-
-//pattern matching + switch expression
-var textInfo = tempInFahrenheit switch
-    {
-        < 32 => "solid",
-        32 => "solid/liquid transition",
-        (> 32) and (< 212) => "liquid",
-        212 => "liquid / gas transition",
-        _ => "unknown"
-    };
-
-
-string message = words[3];
-
-
-string xx = stud._name;
-
-
-
-
+Console.WriteLine($"Načetl jsem {persons.Count} osob.");
