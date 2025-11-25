097727c7f66404355f391b2709f08c4d42a60be9
Kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-11-25 14:35:09 +0100

    LINQ zadani

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -1,9 +1,10 @@
-﻿int[] numbers = { 11, 2, 13, -97542, 44, -5, 6, 127, -99, 0, 256, 0, 12, 11 };
+﻿int[] numbers = { 11, 2, 13, -97542, 44, 
+    -5, 6, 127, -99, 0, 256, 0, 12, 11 };
 
-//// 1.  kolik obsahuje pole kladných èísel
-//// 2.  kolik obsahuje pole záporných èísel
+//// 1.  kolik obsahuje pole kladných čísel
+//// 2.  kolik obsahuje pole záporných čísel
 //// 3.  sumu kladných hodnot
-//// 4.  nejvìtší absolutní hodnotu
-//// 5.  všechna kladná sudá èísla
-//// 6.  seøaïte pole od nejmenších po nejvìtší hodnoty,
-//// 7.  pøeskoète první 3 prvky a seètìte zbytek hodnot
+//// 4.  největší absolutní hodnotu
+//// 5.  všechna kladná sudá čísla
+//// 6.  seřaďte pole od nejmenších po největší hodnoty,
+//// 7.  přeskočte první 3 prvky a sečtěte zbytek hodnot
