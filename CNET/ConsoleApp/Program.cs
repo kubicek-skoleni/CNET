ae7f4d39dd0bb658281bda9e736ddbf91f53a290
Kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-11-25 14:33:20 +0100

    LINQ zadani

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -1,13 +1,69 @@
-﻿using DataAccess;
-using Model;
+﻿int[] numbers = { 10, 23, 5, 8, 15, 42, 7 };
 
-PeopleDbContext context = new PeopleDbContext();
+var suda = numbers.Where(cislo => cislo % 2 == 0);
 
-var addr_count = context.Addresses.Count();
-var contract_count = context.Contracts.Count();
+var ordered = numbers.OrderByDescending(cislo => cislo);
 
-Console.WriteLine($"Adres: {addr_count}");
-Console.WriteLine($"Smluv: {contract_count}");
+var avg = numbers.Average();
+//Console.WriteLine($"Avg: {Math.Round(avg,1)}");
+
+var cnt = numbers.Count();
+//Console.WriteLine($"Count: {cnt}");
+
+var take3 = numbers.Take(3);
+
+var skip3 = numbers.Skip(3);
+
+//Select
+var doubles = numbers.Select(cislo => cislo * 2);
+
+
+//First
+int first = numbers.First();
+
+var result = numbers
+    .Where(cislo => cislo > 15)
+    .OrderByDescending(x => x)
+    .Select(x => x + 100);
+
+foreach (var num in result)
+{
+    Console.WriteLine(num);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using DataAccess;
+//using Model;
+
+//PeopleDbContext context = new PeopleDbContext();
+
+//var addr_count = context.Addresses.Count();
+//var contract_count = context.Contracts.Count();
+
+//Console.WriteLine($"Adres: {addr_count}");
+//Console.WriteLine($"Smluv: {contract_count}");
+
+
+
+
 
 
 
