79ad9b19a6d77bc15e6d63d388f6ec7c93ff4ad8
Kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-11-25 13:39:06 +0100

    PeopleDbContext - SQLite

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -1,11 +1,37 @@
-﻿using System.Text.Json;
-using Model;
+﻿
 
-string file = @"data2024.json";
 
-var file_content = File.ReadAllText(file);
 
-List<Person> persons = JsonSerializer
-                .Deserialize<List<Person>>(file_content)!;
 
-Console.WriteLine($"Načetl jsem {persons.Count} osob.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System.Text.Json;
+//using Model;
+
+//string file = @"data2024.json";
+
+//var file_content = File.ReadAllText(file);
+
+//List<Person> persons = JsonSerializer
+//                .Deserialize<List<Person>>(file_content)!;
+
+//Console.WriteLine($"Načetl jsem {persons.Count} osob.");
+
+
---
+++ CNET/DataAccess/PeopleDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace DataAccess
+{
+    public class PeopleDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<Contract> Contracts { get; set; }
+
+
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={GetDbPath()}");
+        }
+
+        private string GetDbPath()
+        {
+            //return @"C:\PROJECTS\skoleni\people.db";
+
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var local_folder = Environment.GetFolderPath(folder);
+            var path = Path.Join(local_folder, "PeopleDb");
+            if(!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            return Path.Join(path, "people.db");
+        }
+    }
+}
