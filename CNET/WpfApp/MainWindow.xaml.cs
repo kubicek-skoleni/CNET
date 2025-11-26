8f6db8da9966926571a464b77ee0679a282425db
Kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-11-26 11:27:10 +0100

    all async start

--- CNET/WpfApp/MainWindow.xaml.cs
+++ CNET/WpfApp/MainWindow.xaml.cs
@@ -32,17 +32,70 @@ public partial class MainWindow : Window
         Stopwatch time = new Stopwatch();
         time.Start();
 
+        txbInfo.Text = "";
         string dir = @"C:\PROJECTS\skoleni\2025_komfi\BigFiles";
         var files = Directory.GetFiles(dir);
 
         Dictionary<string, int> stats = new();
         foreach (var file in files)
         {
+            var words = File.ReadLines(file);
 
+            foreach (var word in words)
+            {
+                if (stats.ContainsKey(word))
+                    stats[word]++;
+                else
+                    stats[word] = 1;
+            }
+        }
 
+        var top10 = stats.OrderByDescending(kv => kv.Value).Take(10);
+
+        foreach (var kv in top10)
+        {
+            txbInfo.Text += $"{kv.Key}: {kv.Value}{Environment.NewLine}";
         }
 
         time.Stop();
         txbInfo.Text += $"{Environment.NewLine} Time: {time.ElapsedMilliseconds} ms";
+    }
+
+    private void btnAllAsync_Click(object sender, RoutedEventArgs e)
+    {
+        Mouse.OverrideCursor = Cursors.Wait;
+
+        Stopwatch time = new Stopwatch();
+        time.Start();
+
+        txbInfo.Text = "";
+        string dir = @"C:\PROJECTS\skoleni\2025_komfi\BigFiles";
+        var files = Directory.GetFiles(dir);
+
+        Dictionary<string, int> stats = new();
+        foreach (var file in files)
+        {
+            var words = File.ReadLines(file);
+
+            foreach (var word in words)
+            {
+                if (stats.ContainsKey(word))
+                    stats[word]++;
+                else
+                    stats[word] = 1;
+            }
+        }
+
+        var top10 = stats.OrderByDescending(kv => kv.Value).Take(10);
+
+        foreach (var kv in top10)
+        {
+            txbInfo.Text += $"{kv.Key}: {kv.Value}{Environment.NewLine}";
+        }
+
+        time.Stop();
+        txbInfo.Text += $"{Environment.NewLine} Time: {time.ElapsedMilliseconds} ms";
+
+        Mouse.OverrideCursor = null;
     }
 }
